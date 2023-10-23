442bdcf8294144939756305fae6209cb3a1a1996
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-10-23 14:37:31 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Inlämningsuppgifter4._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Skriv in ditt meddelande: ");
+            string meddelande = Console.ReadLine();
+
+            Console.Write("Ange antal steg att flytta åt höger (0 för längst till vänster): ");
+            int steg = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < meddelande.Length; i++)
+            {
+                for (int j = 0; j < steg; j++)
+                {
+                    Console.Write(" ");
+                }
+                Console.WriteLine(meddelande[i]);
+            }
+        }
+    }
+}
